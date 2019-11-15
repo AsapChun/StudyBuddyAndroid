4a205d8f8a5828078f1c5614d30a36ebb558c719
Sean Chun <schun@bu.edu>
2019-11-15 16:10:38 -0500

    settings activity

--- studybuddy/app/src/main/java/com/example/studybuddy/SettingsActivity.java
+++ studybuddy/app/src/main/java/com/example/studybuddy/SettingsActivity.java
@@ -3,18 +3,48 @@ package com.example.studybuddy;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
     private Button btnBack;
+    private Button btnChangeEmail;
+    private Button btnChangePassword;
+    private EditText OldUsername;
+    private EditText NewUsername;
+    private EditText OldPassword;
+    private EditText NewPassword;
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
         btnBack =  (Button) findViewById(R.id.btnNewBack);
+        btnChangeEmail = (Button) findViewById(R.id.btnChangeEmail);
+        btnChangePassword = (Button) findViewById(R.id.btnConfirm);
+        OldUsername = (EditText) findViewById(R.id.edtOldUsername);
+        NewUsername = (EditText) findViewById(R.id.edtNewUsername);
+        OldPassword = (EditText) findViewById(R.id.edtOldPassword);
+        NewPassword = (EditText) findViewById(R.id.edtNewPassword);
+
+        btnChangeEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
 
 
